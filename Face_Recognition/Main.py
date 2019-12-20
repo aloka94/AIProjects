1b01b9122c118080d52e05f763e6bf748182cb7b
aloka <aloka94@gmail.com>
2019-12-20 20:02:03 +0100

    Eye recognition

--- Face_Recognition/Main.py
+++ Face_Recognition/Main.py
@@ -8,17 +8,17 @@ Created on Wed Dec 10 23:03:01 2019
 import cv2
 
 #provide path to the image 
-imgPath = "image.jpg"
-
-
-cascadePath = "haarcascade_frontalface_default.xml"
-cascade = cv2.CascadeClassifier(cascadePath)
-
+imgPath = "maroon.jpg"
+ 
+cascade_face = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+#Can be used for face side profile. Not being used currently
+cascade_side_profile=cv2.CascadeClassifier("haarcascade_profileface.xml")
+cascade_eye = cv2.CascadeClassifier("haarcascade_eye.xml")
 img = cv2.imread(imgPath)
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-faces = cascade.detectMultiScale(
-    gray,
+faces = cascade_face.detectMultiScale(
+    gray_img,
     scaleFactor=1.1,
     minNeighbors=5,
     minSize=(30, 30),
@@ -29,6 +29,11 @@ print("Number of faces: {0}".format(len(faces)))
 
 for (x, y, w, h) in faces:
     cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    gray_eye = gray_img[y:y+h, x:x+w]
+    eye_color = img[y:y+h, x:x+w]
+    eyes = cascade_eye.detectMultiScale(gray_eye)
+    for (ex,ey,ew,eh) in eyes:
+        cv2.rectangle(eye_color,(ex,ey),(ex+ew,ey+eh),(255,0,0),2)
     
-cv2.imshow("Detected Faces", img)
+cv2.imshow("Detected Faces and eyes", img)
 cv2.waitKey(0)
